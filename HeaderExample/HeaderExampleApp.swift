8610f9ef7bab41c8969db27405a655dd0a794af7
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2022-11-05 09:47:59 -0400

    Initial Commit

---
+++ HeaderExample/HeaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExampleApp.swift
+//  HeaderExample
+//
+//  Created by Lucas García on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
